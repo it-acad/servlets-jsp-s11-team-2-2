91aaca6b390c0375555cb711c116241b46a7e359
Mykola <mykola.nahirnyak@gmail.com>
2021-01-05 13:29:35 +0200

    HomeServlet has been added

---
+++ src/main/java/com/softserve/itacademy/servlets/HomeServlet.java
@@ -0,0 +1,15 @@
+package com.softserve.itacademy.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+    }
+}
